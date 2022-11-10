8104f1913adffa62b3976d22dd6184632d036032
kenshi244 <maanukyaan@gmail.com>
2022-11-10 20:29:11 +0300

    File manager developed

--- js/script.js
+++ js/script.js
@@ -6,3 +6,28 @@ burger.addEventListener("click", () => {
     ? burger.classList.add("is-active")
     : burger.classList.remove("is-active");
 });
+
+// File manager
+const menu = document.querySelector(".menu");
+const firstLevel = menu.querySelectorAll(".menu__first-level");
+const secondLevel = menu.querySelectorAll(".menu__second-level");
+const thirdLevel = menu.querySelectorAll(".menu__third-level");
+const fourthLevel = menu.querySelectorAll(".menu__fourth-level");
+
+firstLevel.forEach((el) => {
+  el.querySelector("li div").addEventListener("click", (e) => {
+    !e.target.closest("li").querySelector("ul").classList.contains("active")
+      ? e.target.closest("li").querySelector("ul").classList.add("active")
+      : e.target.closest("li").querySelector("ul").classList.remove("active");
+  });
+});
+
+secondLevel.forEach((el) => {
+  el.querySelectorAll("li div").forEach((div) => {
+    div.addEventListener("click", (e) => {
+      !e.target.closest("li").querySelector("ul").classList.contains("active")
+        ? e.target.closest("li").querySelector("ul").classList.add("active")
+        : e.target.closest("li").querySelector("ul").classList.remove("active");
+    });
+  });
+});
